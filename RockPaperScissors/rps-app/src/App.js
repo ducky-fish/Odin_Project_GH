cdb9b5f351111c5153e2b126f4ba6c3ddaf97f86
Eva Honcho <rutledgetri@gmail.com>
2024-09-19 17:02:22 -0600

    Figured out image load button issue

--- RockPaperScissors/rps-app/src/App.js
+++ RockPaperScissors/rps-app/src/App.js
@@ -1,17 +1,37 @@
 
 import './App.css';
 
+// import {
+//   StyleSheet,
+//   Button,
+//   View,
+//   SafeAreaView,
+//   Text,
+//   Alert,
+// } from 'react';
+
 
 function App() {
-  const playChoice = ['rock','paper','scissors'];
+  const playChoice = ['rock', 'paper', 'scissors'];
   let getComputerChoice = playChoice[Math.floor(Math.random() * playChoice.length)];
-  
+
+
   // const [computerChoice, getComputerChoice] = useState(0);
   return (
-    <div className = "rps">
-      <p>{getComputerChoice}</p>
-    </div>
+    <>
+        <div className="rps">
+          <p>{getComputerChoice}</p>
+        </div>
+        <img  sizes='small'></img>
+        <button type="answer">
+          <img src={require("./Rock.jpeg")} size = "s"></img>
+        </button>
+    </>
   );
+}
+
+function userChoice() {
+
 }
 
 export default App;
