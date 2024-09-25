c9750ab0bb43c6da296ec0baf187b82f932b42cd
Eva Honcho <rutledgetri@gmail.com>
2024-09-25 14:25:42 -0600

    Completed:     +  get the onUserChoice function out of App function        find how to pass set state through to externally        blocked functions
    Next Features:
        - connect machineSelect[1] to a variable name
          clean up variable names
        - Pass set state more cleanly; perhaps as function
        - Make counter of games won and lost

--- RockPaperScissors/rps-app/src/App.js
+++ RockPaperScissors/rps-app/src/App.js
@@ -4,55 +4,56 @@ import { useState } from 'react';
 
 
 function App() {
-  const playChoice = ['Rock', 'Paper', 'Scissors'];
-  const [computerChoice, setComputerChoice] = useState(getComputerChoice(playChoice));
-
-  function onUserChoice(userSelect, machineSelect) {
-    console.log(userSelect.toString());
-    console.log(machineSelect.toString());
-
-    setComputerChoice(getComputerChoice(playChoice));
-
-    if (userSelect === machineSelect) {
-      return alert("No Winner :(");
-    }
-    else if ((userSelect === "Rock" && machineSelect === "Scissors") ||
-      (userSelect === "Paper" && machineSelect === "Rock") ||
-      (userSelect === "Scissors" && machineSelect === "Paper")
-    ) {
-      return alert("Player Wins :)");
-    }
-    else {
-      return alert('Computer Wins :(');
-    }
-  }
+  const [computerChoice, setComputerChoice] = useState(getComputerChoice());
 
   return (
     <>
       <div className="rps">
         <p>{computerChoice}</p>
       </div>
-      {console.log(getComputerChoice)}
-      <button type="answer" onClick={() => onUserChoice("Rock", computerChoice)}>
-        <img src={require("./Rock.jpeg")} width="200" height="200" alt='Rock'></img>
-      </button>
-      <button type="answer" onClick={() => onUserChoice("Paper", computerChoice)}>
-        <img src={require("./Paper.jpeg")} width="200" height="200" alt='Paper'></img>
-      </button>
-      <button type="answer" onClick={() => onUserChoice("Scissors", computerChoice)}>
-        <img src={require("./Scissors.jpeg")} width="200" height="200" alt='Scissors'></img>
-      </button>
       <div>
-
+        <button type="answer" onClick={() => onUserChoice("Rock", [computerChoice,setComputerChoice])}>
+          <img src={require("./Rock.jpeg")} width="200" height="200" alt='Rock'></img>
+        </button>
+        <button type="answer" onClick={() => onUserChoice("Paper", [computerChoice,setComputerChoice])}>
+          <img src={require("./Paper.jpeg")} width="200" height="200" alt='Paper'></img>
+        </button>
+        <button type="answer" onClick={() => onUserChoice("Scissors", [computerChoice,setComputerChoice])}>
+          <img src={require("./Scissors.jpeg")} width="200" height="200" alt='Scissors'></img>
+        </button>
       </div>
+
+
     </>
   );
 }
 
+function onUserChoice(userSelect, machineSelect) {
+  console.log(userSelect.toString());
+  console.log(machineSelect.toString());
+  const currMachineSelect = machineSelect[0];
+  
+  let alertMessage = "";
+  if (userSelect === currMachineSelect) {
+    alertMessage = "No Winner :(";
+  }
+  else if ((userSelect === "Rock" && currMachineSelect === "Scissors") ||
+    (userSelect === "Paper" && currMachineSelect === "Rock") ||
+    (userSelect === "Scissors" && currMachineSelect === "Paper")
+  ) {
+    alertMessage = "Player Wins :)";
+  }
+  else {
+    alertMessage = "Computer Wins :(";
+  }
+  machineSelect[1](getComputerChoice());
+  return alert(alertMessage);
+}
 
 
-function getComputerChoice(choices) {
-  return (choices[Math.floor(Math.random() * choices.length)]).toString();
+function getComputerChoice() {
+  const playChoice = ['Rock', 'Paper', 'Scissors'];
+  return (playChoice[Math.floor(Math.random() * playChoice.length)]).toString();
 }
 
 export default App;
