3f95497c4fbdc1dc9b4744d4c43552f5efc23f18
Eva Honcho <rutledgetri@gmail.com>
2024-09-25 11:41:17 -0600

    Completed:     + Currently works on first render instance
    Next Features:
        - Update computer answer after each game.
          a.k.a once player has entered answer
        - Make counter of games won and lost

--- RockPaperScissors/rps-app/src/App.js
+++ RockPaperScissors/rps-app/src/App.js
@@ -10,28 +10,51 @@ import './App.css';
 //   Alert,
 // } from 'react';
 
-
+/*
+  need to refactor, method is:
+  
+*/
 function App() {
-  const playChoice = ['rock', 'paper', 'scissors'];
-  let getComputerChoice = playChoice[Math.floor(Math.random() * playChoice.length)];
+  const playChoice = ['Rock', 'Paper', 'Scissors'];
+  let getComputerChoice =  (playChoice[Math.floor(Math.random() * playChoice.length)]).toString();
 
-
+  
   // const [computerChoice, getComputerChoice] = useState(0);
+
   return (
     <>
-        <div className="rps">
-          <p>{getComputerChoice}</p>
-        </div>
-        <img  sizes='small'></img>
-        <button type="answer">
-          <img src={require("./Rock.jpeg")} size = "s"></img>
-        </button>
+      <div className="rps">
+        <p>{getComputerChoice}</p>
+      </div>
+      {console.log(getComputerChoice)}
+      <button type="answer" onClick={() => onUserChoice("Rock", getComputerChoice)}>
+        <img src={require("./Rock.jpeg")} width="200" height="200" alt='Rock'></img>
+      </button>
+      <button type="answer" onClick={() => onUserChoice("Paper", getComputerChoice)}>
+        <img src={require("./Paper.jpeg")} width="200" height="200" alt='Paper'></img>
+      </button>
+      <button type="answer" onClick={() => onUserChoice("Scissors", getComputerChoice)}>
+        <img src={require("./Scissors.jpeg")} width="200" height="200" alt='Scissors'></img>
+      </button>
     </>
   );
 }
 
-function userChoice() {
-
+function onUserChoice(userSelect, machineSelect) {
+  console.log(userSelect.toString()); 
+  console.log(machineSelect.toString());
+  if (userSelect === machineSelect) {
+    return alert("No Winner :(");
+  }
+  else if ((userSelect === "Rock" && machineSelect === "Scissors") ||
+    (userSelect === "Paper" && machineSelect === "Rock") || 
+    (userSelect === "Scissors" && machineSelect === "Paper")
+  ) {
+    return alert("Player Wins :)");
+  }
+  else{
+    return alert('Computer Wins :(');
+  }
 }
 
 export default App;
