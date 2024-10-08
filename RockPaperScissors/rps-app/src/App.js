cae05329db859032fdf43a0a71e020475dfe0ba4
Eva Honcho <rutledgetri@gmail.com>
2024-10-08 15:11:36 -0600

    COMPLETED:      + Implements scoring increases      + New anonymous function that handles scoring in App()
    TO-DO:
         - UNSURE if I have implemented encapsulation appropriately
         - Want to refactor when I learn more

--- RockPaperScissors/rps-app/src/App.js
+++ RockPaperScissors/rps-app/src/App.js
@@ -1,73 +1,76 @@
 
+import { render } from '@testing-library/react';
 import './App.css';
 import { useState } from 'react';
 
 
 function App() {
 
-  const [computerChoice, setComputerChoice] = useState(GetComputerChoice);
+  const [userScore, setUserScore] = useState(0);
+  const [computerScore, setComputerScore] = useState(0);
 
-  const computerChoiceSet = [computerChoice, setComputerChoice];
+
+  const handleClick = (e) => {
+    const buttonElement = e.currentTarget.id;
+
+    console.log(buttonElement.toString);
+
+    let whoWins = OnUserChoice(buttonElement);
+
+    if (whoWins == "Player") {
+      setUserScore(userScore + 1);
+      alert("Player Wins :)")
+    }
+    else if (whoWins == "Computer") {
+      setComputerScore(computerScore + 1);
+      alert("Computer Wins :(")
+    }
+    else {
+      alert("Nobody Wins 0_0");
+    }
+    return;
+  }
 
   return (
     <>
-      <div className="rps">
-        <p>{computerChoice}</p>
-        <p>
-          <br />
-          Player   :
-          <br />
-          Computer :
-          <br />
-        </p>
-
-      </div>
       <div>
-        <button type="answer" onClick={() => OnUserChoice("Rock", computerChoiceSet)}>
-          <img src={require("./rps-images/Rock.jpeg")} width="200" height="200" alt='Rock'></img>
-        </button>
-        <button type="answer" onClick={() => OnUserChoice("Paper", computerChoiceSet)}>
-          <img src={require("./rps-images/Paper.jpeg")} width="200" height="200" alt='Paper'></img>
-        </button>
-        <button type="answer" onClick={() => OnUserChoice("Scissors", computerChoiceSet)}>
-          <img src={require("./rps-images/Scissors.jpeg")} width="200" height="200" alt='Scissors'></img>
-        </button>
+        <p>
+          Player : {userScore}
+        </p>
+        <p>
+          Computer : {computerScore}
+        </p>
       </div>
+      <button type="answer" id="Rock" onClick={handleClick}>
+        <img src={require("./rps-images/Rock.jpeg")} width="200" height="200" alt='Rock'></img>
+      </button>
+
+      <button type="answer" id="Paper" onClick={handleClick}>
+        <img src={require("./rps-images/Paper.jpeg")} width="200" height="200" alt='Paper'></img>
+      </button>
+
+      <button type="answer" id="Scissors" onClick={handleClick}>
+        <img src={require("./rps-images/Scissors.jpeg")} width="200" height="200" alt='Scissors'></img>
+      </button>
 
     </>
+
   );
 }
-
-function Scoring(whoWins) {
-  let alertMessage = "";
-  const scoreCount = [0,0];
-
-  switch (whoWins) {
-    case "Player":
-      alertMessage = "Player Wins :)";
-      break;
-    case "Computer":
-      alertMessage = "Computer Wins :("
-      break;
-    case "Nobody":
-      alertMessage = "No Winner 0_0";
-  }
-  return alertMessage;
-}
+// const winner = OnUserChoice({whoScored});
 
 
-function OnUserChoice(userSelect, machineSelect) {
-
-  const computerChoiceCurrent = machineSelect[0];
-  let computerChoiceNext = machineSelect[1];
+function OnUserChoice(userSelect) {
+  let computerChoice = GetComputerChoice();
+  console.log(computerChoice);
   let winner = "";
 
-  if (userSelect === computerChoiceCurrent) {
+  if (userSelect === computerChoice) {
     winner = "Nobody";
   }
-  else if ((userSelect === "Rock" && computerChoiceCurrent === "Scissors") ||
-    (userSelect === "Paper" && computerChoiceCurrent === "Rock") ||
-    (userSelect === "Scissors" && computerChoiceCurrent === "Paper")
+  else if ((userSelect === "Rock" && computerChoice === "Scissors") ||
+    (userSelect === "Paper" && computerChoice === "Rock") ||
+    (userSelect === "Scissors" && computerChoice === "Paper")
   ) {
     winner = "Player";
   }
@@ -75,11 +78,94 @@ function OnUserChoice(userSelect, machineSelect) {
     winner = "Computer";
   }
 
+  console.log("Selection: " + userSelect);
+  console.log("OnUserChoice: " + winner);
 
-
-  computerChoiceNext(GetComputerChoice());
-  return alert(Scoring(winner));
+  return winner;
 }
+
+
+
+function IncreaseOneOrOther(boolValue) {
+  if (boolValue) {
+    return
+  }
+
+}
+
+// function WinnerMessage({whoWins}) {
+//   // const [userScore, setUserScore] = useState(0);
+//   // const [computerScore, setComputerScore] = useState(0);
+//   let alertMessage = "";
+//   switch (whoWins) {
+//     case "Player":
+//       alertMessage = "Player Wins :)";
+//       break;
+//     case "Computer":
+//       alertMessage = "Computer Wins :("
+//       break;
+//     case "Nobody":
+//       alertMessage = "No Winner 0_0";
+//   }
+//   alert(alertMessage);
+// }
+
+
+//person 
+// Scoring needs to know who won so it can update the score
+// so pass in prop 
+
+// function Scoring({ whoScored  }) {
+//   console.log("haaahahah" + whoScored)
+//   const [playerScore, setPlayerScore] = useState(0);
+//   // const [computerScore, setComputerScore] = useState(0);
+
+//   let winner = OnUserChoice({whoScored});
+
+//   let alertMessage = "";
+
+//   console.log(winner);
+// if (winner ==  "Player") {
+//   setPlayerScore(playerScore + 1);
+//   alertMessage = "Player Wins :)";
+// }
+// else if (winner == "Computer") {
+//   setComputerScore(computerScore + 1);
+//   alertMessage = "Computer Wins :("
+// }
+// else {
+//   alertMessage = "No Winner 0_0";
+// }
+
+// switch (whoScored) {
+// case "Player":
+//   setPlayerScore(playerScore + 1);
+//   alertMessage = "Player Wins :)";
+//   break;
+//   case "Computer":
+//     setComputerScore(computerScore + 1);
+//     alertMessage = "Computer Wins :("
+//     break;
+//   case "Nobody":
+//     alertMessage = "No Winner 0_0";
+// }
+
+// return (
+// winner
+//     <>
+//       <div >
+//         <p>
+//           Player: {playerScore}
+//         </p>
+//         <p>
+//           {/* Computer: {computerScore} */}
+//         </p>
+//       </div>
+//     </>
+
+// );
+
+// }
 
 
 function GetComputerChoice() {
