5c3198036e6af2e8ad21bbff37f4a7541f942bb3
Eva Honcho <rutledgetri@gmail.com>
2024-10-28 14:41:34 -0600

    Fixed html not taking up entire height of viewport

--- Etch_a_Sketch/etchsketch/src/App.js
+++ Etch_a_Sketch/etchsketch/src/App.js
@@ -3,22 +3,30 @@ import './App.css';
 
 function App() {
   const rows = [];
-  for (let i = 0; i < 16; i++){
+  for (let i = 0; i < 16; i++) {
     rows.push(<FlexContainer />);
   }
-  
+
   return (
     <>
+      <h1>How to draw 4 babies</h1>
+      <div className='WebContainer'>
+
         {rows}
+
+      </div>
     </>
+
+
+
   );
 }
 
-function FlexContainer(){
+function FlexContainer() {
   return <div className='Container'> <FlexItem /> </div>
-
 }
-function FlexItem(){
+
+function FlexItem() {
   const columns = [];
   for (let j = 0; j < 16; j++) {
     columns.push(<div className='Item'>Ite</div>);
