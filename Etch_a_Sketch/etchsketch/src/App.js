0ff3a6302293d4363856c4c8830da18d9d318482
Eva Honcho <rutledgetri@gmail.com>
2024-10-29 20:24:11 -0600

    Complete - now changes background color for the 'Items' in flexbox that mouse hovers
    Todo - change background only once when needed and dont change back till desired
         - use button to distinguish the change

--- Etch_a_Sketch/etchsketch/src/App.js
+++ Etch_a_Sketch/etchsketch/src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
   const rows = [];
@@ -9,7 +10,11 @@ function App() {
 
   return (
     <div className='WebContainer'>
-      <h1>How to draw 4 babies</h1>
+      <div className='HeaderContainer'>
+        <h1 className='HeaderItem'>How to draw 4 babies</h1>
+        <div className='HeaderItem'><OnOffButtonComponent /></div>
+
+      </div>
 
       {rows}
 
@@ -28,12 +33,43 @@ function FlexContainer() {
 function FlexItem() {
   const columns = [];
   for (let j = 0; j < 16; j++) {
-    columns.push(<div className='Item' >Ite</div>);
+    columns.push(<MyComponent />);
   }
   return columns;
+};
+
+
+function OnOffButtonComponent() {
+  const [isClicked, setIsClicked] = useState(false);
+
+  const handleClick = () => {
+    setIsClicked(!isClicked);
+  };
+
+  return <button className='OnOffButton' >On/Off</button>;
 }
 
-//onMouseEnter={()=> {{backgroundColor: 'black'}}}
+const MyComponent = () => {
+  const [divChangeBackground, setDivChangeBackground] = useState(false);
 
+  const handleMouseEnter = () => {
+    setDivChangeBackground(!divChangeBackground);
+  };
+
+  return (
+    <div
+      className={divChangeBackground ? 'ItemNew' : 'ItemInitial'}
+      onMouseEnter={handleMouseEnter}
+    >
+      Ite
+    </div>
+  );
+};
+
+//making a button on off switch on press that when on (basically taking same logic as the MyComponent)
+//will allow you to draw (it is set ready to draw)
+//    - make another component operation that sets the class name of the div to 'ItemNew'
+//when off will erase what youve drawn
+//    - another componeny operation that reverts the class name
 
 export default App;
